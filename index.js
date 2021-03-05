39867dbdf3762cbd91a3dd13ebc2a1b2439aa171
Jon Newton <newt@hey.com>
2021-03-05 15:57:44 -0500

    Make it TINY

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-'use strict';
-
-const sleep = async (ms) => {
-  return new Promise((resolve) => {
-    setTimeout(resolve, ms);
-  });
+module.exports.sleep = async (ms) => {
+  return new Promise(resolve => setTimeout(resolve, ms));
 };
-
-module.exports.default = sleep;
 
